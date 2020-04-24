b01e432294be577e6cb3d59a1e15eeba92d208c1
Kirk.Sayre <Kirk.Sayre@walmart.com>
2020-04-24 09:49:11 -0500

    Hook up lark parser to xlmulator.

--- XLM/__init__.py
+++ XLM/__init__.py
@@ -7,6 +7,11 @@ from __future__ import print_function
 import subprocess
 import sys
 import re
+import os
+
+# sudo pip install lark-parser
+from lark import Lark
+from lark import UnexpectedInput
 
 import XLM.color_print
 
@@ -16,9 +21,20 @@ import XLM.color_print
 try:
     subprocess.check_output(["olevba", "-h"])
 except Exception as e:
-    print("ERROR: It looks like olevba is not installed. " + str(e) + "\n")
+    color_print.output('r', "ERROR: It looks like olevba is not installed. " + str(e) + "\n")
     sys.exit(101)
 
+## Load the olevba XLM grammar.
+xlm_grammar_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), "olevba_xlm.bnf")
+xlm_grammar = None
+try:
+    f = open(xlm_grammar_file, "r")
+    xlm_grammar = f.read()
+    f.close()
+except IOError as e:
+    color_print.output('r', "ERROR: Cannot read XLM grammar file " + xlm_grammar_file + ". " + str(e))
+    sys.exit(102)
+    
 ####################################################################
 def _extract_xlm(maldoc):
     """
@@ -49,9 +65,9 @@ def _extract_xlm(maldoc):
         # and fix them.
         #
         # ' 0006     72 FORMULA : Cell Formula - R9C1 len=50 ptgRefV R7C49153 ptgStr "Set wsh = CreateObject("WScript.Shell")" ptgFuncV FWRITELN (0x0089) 
-        str_pat = r"ptgStr \"(.*)\" ptg"
+        str_pat = b"ptgStr \"(.*)\" ptg"
         for old_str in re.findall(str_pat, line):
-            if ('"' in old_str):
+            if (b'"' in old_str):
                 new_str = old_str.replace('"', "'")
                 line = line.replace(old_str, new_str)        
         r += line
@@ -69,8 +85,19 @@ def _extract_xlm_objects(xlm_code):
     error
     """
 
-    return None
+    # Parse the olevba XLM.
+    xlm_ast = None
+    try:
+        xlm_parser = Lark(xlm_grammar, start="lines", parser='lalr')
+        xlm_ast = xlm_parser.parse(xlm_code.decode())
+    except UnexpectedInput as e:
+        color_print.output('r', "ERROR: Parsing olevba XLM failed.\n" + str(e))
+        return None
 
+    # Transform the AST into XLM_Object objects.
+    print(xlm_ast.pretty())
+    return None
+     
 ####################################################################
 def emulate(maldoc):
     """
@@ -89,7 +116,6 @@ def emulate(maldoc):
         return []
 
     # Parse the XLM text and get XLM objects that can be emulated.
-    print(xlm_code.decode())
     xlm_object = _extract_xlm_objects(xlm_code)
     if (xlm_object is None):
         color_print.output('r', "Parsing of XLM failed. Emulation aborted.")
